ba0b2adc987b8f67539567914b010fe8474a2fee
Jose Vazquez <vazqjose@gmail.com>
2021-09-16 23:26:36 +0000

    t2

--- 0x00-challenge/2-sort.rb
+++ 0x00-challenge/2-sort.rb
@@ -11,19 +11,24 @@ ARGV.each do |arg|
 
     # convert to integer
     i_arg = arg.to_i
-    
+
     # insert result at the right position
     is_inserted = false
     i = 0
     l = result.size
     while !is_inserted && i < l do
-        if result[i] < i_arg
-            i += 1
+      if result[i] < i_arg
+        i += 1
+      else
+        if i == 0
+          result.insert(0, i_arg)
         else
-            result.insert(i - 1, i_arg)
-            is_inserted = true
-            break
+          result.insert(i, i_arg)
+
         end
+        is_inserted = true
+        break
+      end
     end
     result << i_arg if !is_inserted
 end
