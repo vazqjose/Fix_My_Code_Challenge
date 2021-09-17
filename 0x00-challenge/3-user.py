6e1eb5729c0f8dccda4589fe1f053d35a0f48558
Jose Vazquez <vazqjose@gmail.com>
2021-09-17 00:48:22 +0000

    t3

--- 0x00-challenge/3-user.py
+++ 0x00-challenge/3-user.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
- User Model
+User Model
 """
 import hashlib
 import uuid
@@ -40,7 +40,7 @@ class User():
         if pwd is None or type(pwd) is not str:
             self.__password = None
         else:
-            self._password = hashlib.md5(pwd.encode()).hexdigest().lower()
+            self.__password = hashlib.md5(pwd.encode()).hexdigest().lower()
 
     def is_valid_password(self, pwd):
         """
@@ -86,11 +86,11 @@ if __name__ == '__main__':
 
     if not user_1.is_valid_password(u_pwd):
         print("is_valid_password should return True if it's the right \
-password")
+                password")
 
     if user_1.is_valid_password("Fakepwd"):
         print("is_valid_password should return False if it's not the right \
-password")
+                password")
 
     if user_1.is_valid_password(None):
         print("is_valid_password should return False if compare with None")
@@ -100,4 +100,5 @@ password")
 
     if user_2.is_valid_password("No pwd"):
         print("is_valid_password should return False if no password set \
-before")
+                before")
+
